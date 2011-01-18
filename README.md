1f90c62f6064df58dd4d001df2a6a1d1fe6a9961
Joonas Javanainen <joonas.javanainen@gmail.com>
2011-01-18 11:48:37 +0200

    New: Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+Samsung photo frame control
+=================
+
+A small Python application for controlling Samsung photo frames.
+
+Based on the work of [Grace Woo & others](http://web.media.mit.edu/~gracewoo/stuff/picframe/). One large difference is that my application adds an extra control message that prevents the photo frame from exiting mini display mode.
+
+Features
+--------
+
+* If a photo frame is in mass storage mode, the program will change it into mini display mode.
+* If a photo frame is in mini display mode, the program will send the jpeg that was specified as the program argument to the photo frame. *The JPEG must be prescaled to the exactly correct size!*
+
+Supported photo frames
+----------------------
+
+* SPF-107H (not tested)
+* SPF-87H
+* (In theory) Other similar Samsung photo frames should work once their product IDs are added into the code
+
+Dependencies
+------------
+
+* pyusb 1.0 (This is an alpha quality library so it is *not* the one usually packaged in linux distributions. For example, this is not python-usb in Ubuntu!)
+
+Usage
+-----
+
+`sudo ./frame-ctrl.py my_correctly_scaled_image.jpg`
+
+FAQ
+---
+
+### Can I use my photo frame as a mini monitor in Linux?
+
+Nope. In theory it's possible but it would require an X driver that would repeatedly compress frames into JPEG format and send them to the photo frame. This is exactly what the Frame Manager software does in Windows.
+
+### Why do I need to use sudo?
+
+libusb needs direct access to the usb device and unless you have set up permissions explicitly, you won't have access to the raw usb devices.
---
+++ frame-ctrl.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+import usb.core
+from usb.util import *
+
+vendorId = 0x04e8
+models = {'SPF-87H': (0x2033, 0x2034) }
+
+chunkSize = 0x4000
+bufferSize = 0x20000
+
+def expect(result, verifyList):
+  resultList = result.tolist()
+  if resultList != verifyList:
+    print "Warning: Expected " + str(verifyList) + " but got " + str(resultList)
+
+def storageToDisplay(dev):
+  print "Setting device to display mode"
+  try:
+    dev.ctrl_transfer(CTRL_TYPE_STANDARD | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x06, 0xfe, 0xfe, 254)
+  except usb.core.USBError as e:
+    errorStr = str(e)
+    if errorStr != 'No such device (it may have been disconnected)':
+      raise e
+
+def displayModeSetup(dev):
+  print "Sending setup commands to device"
+  dev.set_configuration()
+  result = dev.ctrl_transfer(CTRL_TYPE_VENDOR | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x04, 0x00, 0x00, 1)
+  expect(result, [ 0x03 ])
+#  result = dev.ctrl_transfer(CTRL_TYPE_VENDOR | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x01, 0x00, 0x00, 2)
+#  expect(result, [ 0x09, 0x04 ])
+#  result = dev.ctrl_transfer(CTRL_TYPE_VENDOR | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x02, 0x00, 0x00, 1)
+#  expect(result, [ 0x46 ])
+
+def chunkyWrite(dev, buf):
+  pos = 0
+  while pos < bufferSize:
+    dev.write(0x02, buf[pos:pos+chunkSize])
+    pos += chunkSize
+
+def writeImage(dev, path):
+#  result = dev.ctrl_transfer(CTRL_TYPE_STANDARD | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x06, 0x0300, 0x00, 255)
+#  expect(result, [ 0x04, 0x03, 0x09, 0x04 ])
+
+#  result = dev.ctrl_transfer(CTRL_TYPE_STANDARD | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x06, 0x0301, 0x0409, 255)
+
+  size = os.path.getsize(path)
+  f = open(path)
+
+  buf = bytearray(bufferSize)
+
+  header = bytearray([ 0xa5, 0x5a, 0x09, 0x04, size & 0xff, size >> 8 & 0xff, size >> 16 & 0xff, size >> 24 & 0xff, 0x46, 0x00, 0x00, 0x00 ])
+
+  headerSize = len(header)
+
+  data = f.read(bufferSize - headerSize)
+  buf[0:headerSize] = header
+  buf[headerSize:headerSize + len(data)] = data
+  chunkyWrite(dev, buf)
+  
+  while True:
+    data = f.read(bufferSize)
+    if data == "":
+      break    
+    buf[0:len(data)] = data
+    for i in xrange(len(data), len(buf)):
+      buf[i] = 0
+    chunkyWrite(dev, buf)
+
+#  result = dev.ctrl_transfer(CTRL_TYPE_VENDOR | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x06, 0x00, 0x00, 2)
+#  expect(result, [ 0x00, 0x00 ])
+
+  f.close()
+
+for k, v in models.iteritems():
+  dev = usb.core.find(idVendor=vendorId, idProduct=v[0])
+  if dev:
+    print "Found " + k + " in storage mode"
+    storageToDisplay(dev)
+    time.sleep(1)
+  dev = usb.core.find(idVendor=vendorId, idProduct=v[1])
+  if dev:
+    print "Found " + k + " in display mode"
+    displayModeSetup(dev)
+    writeImage(dev, sys.argv[1])
